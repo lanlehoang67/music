c33ac8e0a3d762302aab016bd1a50f2f285a09e2
lanlehoang10 <lanlehoang10@gmail.com>
2019-03-28 22:18:27 +0700

    fix asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,12 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.precompile += %w( songs_show.js )
+Rails.application.config.assets.precompile += %w( songs_show.css )
+Rails.application.config.assets.precompile += %w( songs_index.js )
+Rails.application.config.assets.precompile += %w( songs_index.css )
+Rails.application.config.assets.precompile += %w( static_pages.js )
+Rails.application.config.assets.precompile += %w( static_pages.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
