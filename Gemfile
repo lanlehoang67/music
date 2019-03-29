900b0de777eeb0309365b687fd298e21d8f88ca5
lan <lanlehoang10@gmail.com>
2019-03-29 08:47:26 +0700

    finish admin

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
-
+gem 'rails_admin'
 gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 gem 'bootstrap'
 gem 'mysql2', '>= 0.4.4', '< 0.6.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_070015) do
+ActiveRecord::Schema.define(version: 2019_03_29_014240) do
 
   create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "artist_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_03_28_070015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "picture"
     t.index ["album_id"], name: "index_songs_on_album_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["user_id"], name: "index_songs_on_user_id"
